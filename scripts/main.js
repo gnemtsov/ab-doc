537c98004d83f4e5a9e85400dff2d1f4b1240f86
Igor <igor@WorkVM>
2017-07-24 02:00:08 +0300

    Tree: drag&drop

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,75 @@ var poolData = {
 
 var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
 
+// zTree \/
+var settings = {
+	view: {
+		selectedMulti: false
+	},
+	edit: {
+		enable: true,
+		showRemoveBtn: false,
+		showRenameBtn: false,
+		drag: {
+			isCopy: true,
+			isMove: true,
+			prev: true,
+			inner: true,
+			next: true
+		}
+	},
+	data: {
+		simpleData: {
+			enable: true
+		}
+	},
+	callback: {
+		beforeDrag: beforeDrag,
+		beforeDrop: beforeDrop,
+		// For testing!
+		onDrop: function (event, treeId, treeNodes, targetNode, moveType, isCopy) {
+			console.log(targetNode);
+			console.log(moveType);
+		}
+	}
+};
+
+function beforeDrag(id, nodes) {
+	var ok = true;
+	nodes.forEach(function(x, i, arr) {
+		// We can't drag head of the tree (node with username)
+		if (x.head === true) {
+			ok = false;
+		}
+	});
+	
+	return ok;
+}
+
+function beforeDrop (id, nodes, targetNode, moveType) {
+	// We can't drop item out the tree
+	if (targetNode === null) {
+		return false;
+	}
+	// If targetNode is the head (username), there are 3 possible situations:
+	// 1, 2: dropping before it or after it - not ok
+	// 3: dropping inside it - ok
+	if (targetNode.head && moveType != "inner") {
+		return  false;
+	}
+	return true;
+}
+
+var zNodes = [
+	{ id: 1, pId: 0, name: "username", open: true, head: true},
+	{ id: 11, pId: 1, name: "item1"},
+	{ id: 12, pId: 1, name: "item2"},
+	{ id: 13, pId: 1, name: "folder1", open: true},
+	{ id: 131, pId: 13, name: "item3"}
+];
+
+// zTree /\
+
 $(document).ready( function() {
     var cognitoUser = userPool.getCurrentUser();
     
@@ -22,4 +91,6 @@ $(document).ready( function() {
 		cognitoUser.signOut();
 		return true;	
 	});	
+	
+	$.fn.zTree.init($("#abTree"), settings, zNodes);
 });
