7e85e6407a62d9b70accc917ea40f7d71b866124
Igor <buranovigor@gmail.com>
2017-09-01 13:23:33 +0300

    #1722 Simplified code

--- scripts/init-translator.js
+++
@@ -1,8 +0,0 @@
-function initTranslator(callback) {
-	translator.initFromLocalStorage();
-
-	$.getJSON("/scripts/translator-data.json", function(data) {
-		translator.setData(data);
-		callback();
-	});
-}
--- scripts/translate-main.js
+++
@@ -1,7 +0,0 @@
-function translatePage() {
-	$("#linkSignOut").html(translator.translate("exit"));
-	$("#spanSomethingWentWrong").html(translator.translate("somethingWentWrong"));
-	$("#linkReturn").html(translator.translate("return"));
-	$("#spanDocument").html(translator.translate("document"));
-}
-
