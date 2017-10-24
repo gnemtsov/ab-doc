980863542ab61cbec238bf67ca2e4f4008b9dede
Igor <buranovigor@gmail.com>
2017-10-24 14:03:29 +0300

    #1800 Changed preloader when file is deleted

--- scripts/ab/my.tmp.js
+++ scripts/ab/my.tmp.js
@@ -920,7 +920,8 @@ function initQuill(id, guid, ownerid, readOnly) {
 				
 				console.log('Removing ', key);
 				
-				$li.html('<div class="small-preloader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>');
+				$li.find('.file-question')
+					.html('<div class="small-preloader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>');
 				$files.attr('waiting', Number($files.attr('waiting')) + 1);
 				$updated.show().addClass('pending');
 
