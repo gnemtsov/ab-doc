ca09b2caa56056fff4f6277f5c3823156f581ee1
Igor <buranovigor@gmail.com>
2018-02-05 17:39:43 +0300

    #2018 Fixed XSS in Quill (maybe not completely)

--- scripts/doc.js
+++ scripts/doc.js
@@ -714,7 +714,7 @@
 					}
 				};
 
-				$editor.html(html);
+				$editor.html($.parseHTML(html));
 				self.editor = new Quill('#editor', editor_options);
 				var length = self.editor.getLength();
 				if (self.editor.getText(length - 2, 2) === '\n\n') {
