58fc5b32eb28cab205c4c61a1080ad5ec4e7a7c3
Igor <buranovigor@gmail.com>
2018-02-28 17:15:53 +0300

    #2051 top trees (not ready yet)

--- scripts/about.js
+++ scripts/about.js
@@ -2,9 +2,6 @@
 "use strict";
 
 (function (g, $) {
-	var topTrees = [],
-		$treeList = $('#todo'); // TODO
-	
 	abUtils.listS3Files('')
 		.then( function(files) {
 			// TODO: 
@@ -21,6 +18,10 @@
 			
 			console.log(trees);
 			
+			var $topTreesDiv = $('#top-trees'),
+				topTreesChildren = [],
+				topTreesSizes = [];
+			
 			trees.forEach( function(tree) {
 				var params = {
 					Bucket: STORAGE_BUCKET,
@@ -29,9 +30,28 @@
 				s3.getObject(params).promise()
 					.then( function(obj) {
 						var treeJson = JSON.parse(obj.Body.toString('utf-8'));
+						var name = tree.owner,
+							size = numberOfSubNodes(treeJson[0]);
 						
-						console.log(treeJson);
+						var newTreeHtml = '<div>' + name + ' | ' + size + '</div>';
+						var pos = topTreesSizes.findIndex( function(s) {
+							return size > s;
+						});
+						if (pos > -1) {
+							topTreesSizes.splice(pos, 0, size);
+						} else {
+							topTreesSizes.push(size);
+						}
+						
+						console.log(topTreesSizes);
 					});
 			});
 		});
+		
+	function numberOfSubNodes(node) {
+		var n = node.children.length;
+		return node.children.reduce( function(acc, c) {
+			return acc + numberOfSubNodes(c);
+		}, n);
+	}
 }(window, jQuery));
